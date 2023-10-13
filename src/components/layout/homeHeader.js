078ffbda3fa5c506eb4fb44e3188ef4b27169d52
Sarah Ann Garcia <sarahanngarcia30@gmail.com>
2023-10-13 16:28:36 -0400

    feat: update header styles and add a CTA with hover styles

--- src/components/layout/homeHeader.js
+++ src/components/layout/homeHeader.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 
 import './styles/homeHeader.scss';
@@ -11,21 +12,35 @@ class HomeHeadeer extends React.Component {
         <Grid
           container
           direction="row"
-          justify="center"
-          alignItems="center"
+          justify="left"
+          alignItems="left"
         >
         <div className="header-img">
           <div className="intro">
             <div>
-              <h1 className="main-intro">
-                <strong>Sarah Ann Garcia</strong>
-              </h1>
-              <hr />
+              <p className="bio-blurb">
+                Sarah Ann Garcia.
+              </p>
+            </div>
+            <div>
+              <div className="main-intro">
+                <strong>Solving Complex Application</strong>
+              </div>
+              <div className="main-intro">
+                <strong>Design Challenges</strong>
+              </div>
             </div>
             <div>
               <p className="bio-blurb">
-                Web Engineer
+                With <em>dedication</em>, <em>perseverance</em>, and <em>integrity</em>.
               </p>
+            </div>
+            <div>
+            <a href='/portfolio/sarahanngarcia_resume.pdf' rel="noopener noreferrer" target="_blank" className="btn mx-auto view-site">
+              <Button className="cta-button">
+                View Resume
+              </Button>
+              </a>
             </div>
           </div>
         </div>
