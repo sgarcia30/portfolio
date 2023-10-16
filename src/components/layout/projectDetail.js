ebed0544a3e41afb350903e14fd353942f6ec876
Sarah Ann Garcia <sarahanngarcia30@gmail.com>
2023-10-16 17:02:11 -0400

    WIP: updating the project card styles, still needs work

--- src/components/layout/projectDetail.js
+++ src/components/layout/projectDetail.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import { Card, CardContent } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import { projects } from '../../data/projects.data.json';
 import './styles/projectDetail.scss';
@@ -8,16 +9,31 @@ class ProjectDetail extends React.Component {
   render () {
     return (
       <section id="project_links">
+        <div className="title">
+          Projects
+        </div>
         <div className="container mx-auto px-0 align-content-">
           <Grid container spacing={24} className="grid-container">
-          { projects.map((project, key) => {
+          {projects.map((project, key) => {
             return (
-              <Grid key={key} item xs={12} sm={6} className="imageHover">
+              <Grid key={key} item xs={12} sm={12} className="">
                 <Link to={`/portfolio/work/${project.slug}`}>
-                  <img src={project.src} alt={project.alt} className="img-fluid desaturate" />
-                  <div className="overlay"></div>
-                  <h2 className="project_name text-center">{project.projectName}</h2>
-                  <div className="logo-container"><img src={project.logo} alt={project.logoAlt} className="project_logo" /></div>
+                  <Card className='project-container'>
+                    <CardContent className='project-content'>
+                      <div className='project-media'>
+                        <img src={project.src} alt={project.alt} className="project-bg-img"/>
+                        <div className="overlay"></div>
+                        <div className="logo-container">
+                          <img src={project.logo} alt={project.logoAlt} className="project_logo"/>
+                        </div>
+                      </div>
+                      <div className='project-info'>
+                        <div className="project_name">{project.projectName}</div>
+                        <div></div>
+                        <div></div>
+                      </div>
+                    </CardContent>
+                  </Card>
                 </Link>
               </Grid>
             );
